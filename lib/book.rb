ebe26262c8f4815bec919d18fa83d2e26b9230b5
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-12-18 10:59:56 -0700

    Add library class and tests

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book
 
 attr_reader :title, :author, :publication_year
+attr_accessor :checked_out
 
   def initialize(book_attributes) 
     @title = book_attributes[:title] 
     @author = book_attributes[:author_first_name] + " " + book_attributes[:author_last_name]
     @publication_year = book_attributes[:publication_date][-4..-1]
+    @checked_out = false
   end 
   
 end
--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,13 @@
 class Library
 
-  attr_reader :name, :books, :authors
+  attr_reader :name, :books, :authors, :checked_out_books, :check_out_count
 
   def initialize(name) 
     @name = name 
     @books = []
     @authors = []
+    @checked_out_books = []
+    @check_out_count = Hash.new(0)
   end 
 
   def add_author(author)
@@ -16,6 +18,38 @@ class Library
 
   def publication_time_frame_for(author)
     {:start=> author.books_publication_year.min, :end=> author.books_publication_year.max}
+  end
+
+  def checkout(book)
+    return false if !books.include?(book) 
+
+    if !book.checked_out 
+      book.checked_out = true
+      @checked_out_books << book
+      check_out_log(book)
+      true
+    else
+      false
+    end
+  end
+
+  def return(book)
+    book.checked_out = false    
+    checked_out_books.delete_at(checked_out_books.index(book))
+  end
+
+  def check_out_log(book)
+    @check_out_count[book] += 1
+    # if !@check_out_count[book]
+    #   @check_out_count[book] = 1
+    # else
+    #   @check_out_count[book] += 1
+    # end
+  end
+
+  def most_popular_book
+    result = @check_out_count.max_by{|book,count| count}
+    result[0]
   end
   
 end
